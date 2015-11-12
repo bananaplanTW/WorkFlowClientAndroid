948a28938fd92d7f643cd6a31b35d3d62ea8cce5
logicmelody <logicmelody@gmail.com>
2015-11-12 20:23:58 +0800

    [RecordTask] Title in action bar

--- app/src/main/java/com/nicloud/workflowclientandroid/record/RecordTaskActivity.java
+++ app/src/main/java/com/nicloud/workflowclientandroid/record/RecordTaskActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.nicloud.workflowclientandroid.R;
 
@@ -13,6 +14,9 @@ public class RecordTaskActivity extends AppCompatActivity {
 
     private ActionBar mActionBar;
     private Toolbar mToolbar;
+
+    private TextView mRecordTaskName;
+    private TextView mRecordCaseName;
 
 
     @Override
@@ -25,10 +29,13 @@ public class RecordTaskActivity extends AppCompatActivity {
     private void initialize() {
         findViews();
         setupActionBar();
+        setupViews();
     }
 
     private void findViews() {
         mToolbar = (Toolbar) findViewById(R.id.tool_bar);
+        mRecordTaskName = (TextView) findViewById(R.id.record_task_name);
+        mRecordCaseName = (TextView) findViewById(R.id.record_case_name);
     }
 
     private void setupActionBar() {
@@ -39,6 +46,11 @@ public class RecordTaskActivity extends AppCompatActivity {
             mActionBar.setDisplayShowTitleEnabled(false);
             mActionBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    private void setupViews() {
+        mRecordTaskName.setText("伺服器服務開發");
+        mRecordCaseName.setText("流程管理專案");
     }
 
     @Override
