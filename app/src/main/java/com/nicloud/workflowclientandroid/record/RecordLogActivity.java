7a842c85c182ce6017c8d70394a485ba83bf12bf
logicmelody <logicmelody@gmail.com>
2015-11-15 14:51:25 +0800

    [RecordLog] Tab in record log page

--- app/src/main/java/com/nicloud/workflowclientandroid/record/RecordLogActivity.java
+++ app/src/main/java/com/nicloud/workflowclientandroid/record/RecordLogActivity.java
@@ -1,23 +1,55 @@
 package com.nicloud.workflowclientandroid.record;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 import com.nicloud.workflowclientandroid.R;
 
-public class RecordLogActivity extends AppCompatActivity {
+public class RecordLogActivity extends AppCompatActivity implements TabHost.OnTabChangeListener {
+
+    private static final class TabTag {
+        public static final String TEXT = "tag_tab_text";
+        public static final String PHOTO = "tag_tab_photo";
+        public static final String FILE = "tag_tab_file";
+    }
 
     private ActionBar mActionBar;
     private Toolbar mToolbar;
 
     private TextView mRecordTaskName;
     private TextView mRecordCaseName;
+
+    private TabHost mRecordLogTabHost;
+
+    private class RecordLogTabContentFactory implements TabHost.TabContentFactory {
+
+        private Context mContext;
+
+        public RecordLogTabContentFactory(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        public View createTabContent(String tag) {
+            View v = new View(mContext);
+            v.setMinimumWidth(0);
+            v.setMinimumHeight(0);
+            v.setVisibility(View.GONE);
+            return v;
+        }
+
+    }
 
 
     @Override
@@ -30,13 +62,14 @@ public class RecordLogActivity extends AppCompatActivity {
     private void initialize() {
         findViews();
         setupActionBar();
-        setupViews();
+        setupTabs();
     }
 
     private void findViews() {
         mToolbar = (Toolbar) findViewById(R.id.tool_bar);
-        mRecordTaskName = (TextView) findViewById(R.id.add_record_task_name);
-        mRecordCaseName = (TextView) findViewById(R.id.add_record_case_name);
+        mRecordTaskName = (TextView) findViewById(R.id.record_log_task_name);
+        mRecordCaseName = (TextView) findViewById(R.id.record_log_case_name);
+        mRecordLogTabHost = (TabHost) findViewById(R.id.record_log_tab_host);
     }
 
     private void setupActionBar() {
@@ -47,11 +80,41 @@ public class RecordLogActivity extends AppCompatActivity {
             mActionBar.setDisplayShowTitleEnabled(false);
             mActionBar.setDisplayHomeAsUpEnabled(true);
         }
-    }
 
-    private void setupViews() {
         mRecordTaskName.setText("伺服器服務開發");
         mRecordCaseName.setText("流程管理專案");
+    }
+
+    private void setupTabs() {
+        mRecordLogTabHost.setup();
+        addTab(TabTag.TEXT);
+        addTab(TabTag.PHOTO);
+        addTab(TabTag.FILE);
+        mRecordLogTabHost.setOnTabChangedListener(this);
+    }
+
+    private void addTab(String tag) {
+        View tabView = getTabView(tag);
+        mRecordLogTabHost.addTab(mRecordLogTabHost.newTabSpec(tag).setIndicator(getTabView(tag))
+                         .setContent(new RecordLogTabContentFactory(this)));
+    }
+
+    private View getTabView(String tag) {
+        View tabView = LayoutInflater.from(this).inflate(R.layout.record_log_tab, null);
+        TextView tabText = (TextView) tabView.findViewById(R.id.record_log_tab_text);
+
+        String text = "";
+        if(TabTag.TEXT.equals(tag)) {
+            text = getString(R.string.record_log_tab_text);
+        } else if(TabTag.PHOTO.equals(tag)) {
+            text = getString(R.string.record_log_tab_photo);
+        } else if(TabTag.FILE.equals(tag)) {
+            text = getString(R.string.record_log_tab_file);
+        }
+
+        tabText.setText(text);
+
+        return tabView;
     }
 
     @Override
@@ -78,5 +141,10 @@ public class RecordLogActivity extends AppCompatActivity {
 
     private void goToAddRecordActivity() {
         startActivity(new Intent(this, AddRecordActivity.class));
+    }
+
+    @Override
+    public void onTabChanged(String tabId) {
+
     }
 }
