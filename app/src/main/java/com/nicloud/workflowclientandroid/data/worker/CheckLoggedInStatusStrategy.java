9d9988b95e0dcd827d1985b7224d44b3b843534c
logicmelody <logicmelody@gmail.com>
2015-11-17 11:08:00 +0800

    [Login] Add login page

---
+++ app/src/main/java/com/nicloud/workflowclientandroid/data/worker/CheckLoggedInStatusStrategy.java
@@ -0,0 +1,45 @@
+package com.nicloud.workflowclientandroid.data.worker;
+
+import android.util.Log;
+
+import com.nicloud.workflowclientandroid.data.data.WorkingData;
+import com.nicloud.workflowclientandroid.data.loading.LoadingDataUtils;
+import com.nicloud.workflowclientandroid.data.restful.IGetRequestStrategy;
+import com.nicloud.workflowclientandroid.data.utility.RestfulUtils;
+import com.nicloud.workflowclientandroid.data.utility.URLUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+/**
+ * Created by daz on 10/20/15.
+ */
+public class CheckLoggedInStatusStrategy implements IGetRequestStrategy {
+
+    private static final String TAG = CheckLoggedInStatusStrategy.class.toString();
+
+    @Override
+    public JSONObject get() {
+        try {
+            HashMap<String, String> headers = new HashMap<>();
+            headers.put("x-user-id", WorkingData.getUserId());
+            headers.put("x-auth-token", WorkingData.getAuthToken());
+
+            String urlString = URLUtils.buildURLString(LoadingDataUtils.WorkingDataUrl.BASE_URL, LoadingDataUtils.WorkingDataUrl.EndPoints.LOGIN_STATUS, null);
+            String responseJSONString = RestfulUtils.restfulGetRequest(urlString, headers);
+            JSONObject responseJSON = new JSONObject(responseJSONString);
+            if (responseJSON.getString("status").equals("success")) {
+                return responseJSON.getJSONObject("result");
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "Exception in CheckLoggedInStatusStrategy()");
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
---
+++ app/src/main/java/com/nicloud/workflowclientandroid/login/LoginActivity.java
@@ -0,0 +1,169 @@
+package com.nicloud.workflowclientandroid.login;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.nicloud.workflowclientandroid.R;
+import com.nicloud.workflowclientandroid.data.data.WorkingData;
+import com.nicloud.workflowclientandroid.data.worker.CheckLoggedInStatusCommand;
+import com.nicloud.workflowclientandroid.data.worker.UserLoggingInCommand;
+import com.nicloud.workflowclientandroid.main.MainActivity;
+
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener,
+        CheckLoggedInStatusCommand.OnFinishCheckingLoggedInStatusListener,
+        UserLoggingInCommand.OnFinishLoggedInListener {
+
+    public static final long NICLOUD_LOGO_DISPLAYING_INTERVAL = 1000L;
+
+    private SharedPreferences mSharedPreferences;
+
+    private LinearLayout mLoginViewContainer;
+
+    private EditText mAccountEditText;
+    private EditText mPasswordEditText;
+    private Button mLoginButton;
+
+    private ImageView mNiCloudImage;
+    private View mNICContainer;
+    private Button mNICRetryButton;
+
+    private Animation mFadeInAnimation;
+    private Animation mFadeOutAnimation;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initialize();
+    }
+
+    private void initialize() {
+        mSharedPreferences = getSharedPreferences(WorkingData.SHARED_PREFERENCE_KEY, 0);
+        mFadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.abc_fade_in);
+        mFadeOutAnimation = AnimationUtils.loadAnimation(this, R.anim.abc_fade_out);
+        findViews();
+        setupViews();
+        hideAllViews();
+    }
+    private void findViews () {
+        mNiCloudImage = (ImageView) findViewById(R.id.login_nicloud_image);
+        mLoginViewContainer = (LinearLayout) findViewById(R.id.login_container);
+        mAccountEditText = (EditText) findViewById(R.id.login_account_edit_text);
+        mPasswordEditText = (EditText) findViewById(R.id.login_password_edit_text);
+        mLoginButton = (Button) findViewById(R.id.login_button);
+        mNICContainer = findViewById(R.id.no_internet_connection_container);
+        mNICRetryButton = (Button) findViewById(R.id.no_internet_connection_retry_button);
+    }
+
+    private void setupViews () {
+        mLoginButton.setOnClickListener(this);
+        mNICRetryButton.setOnClickListener(this);
+    }
+
+    private void hideAllViews () {
+        mLoginViewContainer.setVisibility(View.GONE);
+    }
+
+    private void showAllViews () {
+        mLoginViewContainer.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        WorkingData.setUserId(mSharedPreferences.getString(WorkingData.USER_ID, ""));
+        WorkingData.setAuthToken(mSharedPreferences.getString(WorkingData.AUTH_TOKEN, ""));
+
+        checkLoggedInStatus();
+    }
+
+    private void checkLoggedInStatus() {
+        CheckLoggedInStatusCommand checkLoggedInStatusCommand = new CheckLoggedInStatusCommand(this, this);
+        checkLoggedInStatusCommand.execute();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        overridePendingTransition(0, 0);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login_button:
+                String username = mAccountEditText.getText().toString();
+                String password = mPasswordEditText.getText().toString();
+                UserLoggingInCommand userLoggingInCommand = new UserLoggingInCommand(this, username, password, this);
+                userLoggingInCommand.execute();
+
+                break;
+
+            case R.id.no_internet_connection_retry_button:
+                mNICContainer.startAnimation(mFadeOutAnimation);
+                mNiCloudImage.startAnimation(mFadeInAnimation);
+                mNICContainer.setVisibility(View.GONE);
+                mNiCloudImage.setVisibility(View.VISIBLE);
+                checkLoggedInStatus();
+
+                break;
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        goToMainActivity();
+    }
+
+    private void goToMainActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        //overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        finish();
+    }
+
+    @Override
+    public void onLoggedOut(boolean isFailCausedByInternet) {
+        if (isFailCausedByInternet) {
+            mNICContainer.startAnimation(mFadeInAnimation);
+            mNiCloudImage.startAnimation(mFadeOutAnimation);
+            mNICContainer.setVisibility(View.VISIBLE);
+            mNiCloudImage.setVisibility(View.GONE);
+        } else {
+            showAllViews();
+        }
+    }
+
+    @Override
+    public void onLoggedInSucceed(String userId, String authToken) {
+        mSharedPreferences.edit().putString(WorkingData.USER_ID, userId).putString(WorkingData.AUTH_TOKEN, authToken).commit();
+        WorkingData.setUserId(userId);
+        WorkingData.setAuthToken(authToken);
+
+        //ParsePush.subscribeInBackground("user_" + userId);
+
+        goToMainActivity();
+    }
+
+    @Override
+    public void onLoggedInFailed(boolean isFailCausedByInternet) {
+        if (isFailCausedByInternet) {
+            Toast.makeText(this, getString(R.string.no_internet_connection), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, getString(R.string.error_account_password), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
