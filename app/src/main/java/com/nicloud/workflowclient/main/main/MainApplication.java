d54766a0709b30b77d560a111e284d7d9c3ad184
logicmelody <logicmelody@gmail.com>
2015-12-01 17:53:42 +0800

    [Parse] Receive notifications from Parse

--- app/src/main/java/com/nicloud/workflowclient/main/main/MainApplication.java
+++ app/src/main/java/com/nicloud/workflowclient/main/main/MainApplication.java
@@ -4,6 +4,10 @@ import android.app.Application;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
+import com.nicloud.workflowclient.R;
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+
 
 /**
  * @author Danny Lin
@@ -18,7 +22,22 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        initialize();
+    }
+
+    private void initialize() {
+        setupAnimations();
+        setupParse();
+    }
+
+    private void setupAnimations() {
         sFadeInAnimation = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
         sFadeOutAnimation = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
+    }
+
+    private void setupParse() {
+        Parse.initialize(this,
+                getString(R.string.parse_nicloud_application_id), getString(R.string.parse_nicloud_application_client_key));
+        ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 }
